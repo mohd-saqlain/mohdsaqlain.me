68a513854caed15f5e2ba87ff7c6895d6237a02c
Mohd Saqlain <mohdsaqlain6017@gmail.com>
2024-12-31 00:05:21 +0530

    updated linkedin

--- src/components/linkedin_badge.tsx
+++ src/components/linkedin_badge.tsx
@@ -1,6 +1,6 @@
 export default function LinkedInBadge() {
   return (
-<div className="badge-base mt-40 LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="aravsanj" data-version="v1"><a className="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/mohd-saqlain?trk=profile-badge">Mohd Saqlain</a></div>
+<div className="badge-base mt-40 LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="mohd-saqlain-webdev" data-version="v1"><a className="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/mohd-saqlain?trk=profile-badge">Mohd Saqlain</a></div>
               
   );
 }
